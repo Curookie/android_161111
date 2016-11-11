52e485e6af2aa64aa8a896950a22db6999e84072
LBH <dlqudgml0503@naver.com>
2016-11-11 15:40:05 +0900

    레이팅 구현

--- app/src/main/java/com/example/a403/a201344052/MainActivity.java
+++ app/src/main/java/com/example/a403/a201344052/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
+import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     RadioButton rb1;
     RadioButton rb2;
 
+    RatingBar ratb1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,22 +57,22 @@ public class MainActivity extends AppCompatActivity {
         cb1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String str = null;
+                String str = "";
                 if(cb1.isChecked()) str+=cb1.getText()+" ";
                 if(cb2.isChecked()) str+=cb2.getText()+" ";
-                if(str.equals(null)) Toast.makeText(getApplicationContext(),"아무 것도 선택 되지 않았습니다.",Toast.LENGTH_SHORT).show();
+                if(str.equals("")) Toast.makeText(getApplicationContext(),"아무 것도 선택 되지 않았습니다.",Toast.LENGTH_SHORT).show();
                 else Toast.makeText(getApplicationContext(),str+"가 선택 되었습니다.",Toast.LENGTH_SHORT).show();
             }
         });
 
-        cb2 = (CheckBox)findViewById(R.id.checkBox);
+        cb2 = (CheckBox)findViewById(R.id.checkBox2);
         cb2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String str = null;
+                String str = "";
                 if(cb1.isChecked()) str+=cb1.getText()+" ";
                 if(cb2.isChecked()) str+=cb2.getText()+" ";
-                if(str.equals(null)) Toast.makeText(getApplicationContext(),"아무 것도 선택 되지 않았습니다.",Toast.LENGTH_SHORT).show();
+                if(str.equals("")) Toast.makeText(getApplicationContext(),"아무 것도 선택 되지 않았습니다.",Toast.LENGTH_SHORT).show();
                 else Toast.makeText(getApplicationContext(),str+"가 선택 되었습니다.",Toast.LENGTH_SHORT).show();
             }
         });
@@ -84,12 +86,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        rb2 = (RadioButton) findViewById(R.id.radioButton);
+        rb2 = (RadioButton) findViewById(R.id.radioButton2);
         rb2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(rb1.isChecked()) Toast.makeText(getApplicationContext(),rb1.getText()+"가 선택 되었습니다.",Toast.LENGTH_SHORT).show();
                 else Toast.makeText(getApplicationContext(),rb2.getText()+"가 선택 되었습니다.",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ratb1 = (RatingBar) findViewById(R.id.ratingBar);
+        ratb1.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                Toast.makeText(getApplicationContext(),ratb1.getRating()+"점을 주셨습니다.",Toast.LENGTH_SHORT).show();
             }
         });
 
