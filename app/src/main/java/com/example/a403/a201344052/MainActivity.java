8513ba85ab538f2b3d5fb3d5106aac45bdbff9e3
LBH <dlqudgml0503@naver.com>
2016-11-11 14:58:34 +0900

    인사하는 버튼 추가

--- app/src/main/java/com/example/a403/a201344052/MainActivity.java
+++ app/src/main/java/com/example/a403/a201344052/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.a403.a201344052;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = (Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"안녕하세요",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
